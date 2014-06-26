5a6b4dc81aeec53e03cdab6c94b5b96a05ba9779
Sam Carr <samcarr@me.com>
2014-06-26 22:26:39 +0100

    Factor out initial SocialNet used in tests and test multiple messages psoted.

--- src/test/scala/samcarr/socialnet/SocialNetSpec.scala
+++ src/test/scala/samcarr/socialnet/SocialNetSpec.scala
@@ -4,14 +4,19 @@ import org.scalatest._
 
 class SocialNetSpec extends UnitSpec {
   
-  "SocialNet" should "show zero messages for a user that has never posted" in {
-    val socialNet = SocialNet()
-    assert(socialNet.read(Bob) === List())
+  val emptyNet = SocialNet()
+  
+  "SocialNet" should "list zero messages for a user that has never posted" in {
+    assert(emptyNet.read(Bob) === List())
   }
   
   it should "allow a user to post a message and read it back" in {
-    val socialNet = SocialNet()
-    val updatedNet = socialNet.post(Bob, "A message.")
+    val updatedNet = emptyNet.post(Bob, "A message.")
     assert(updatedNet.read(Bob) == List("A message."))
+  }
+  
+  it should "list posted messages for a user, newest first" in {
+    val updatedNet = emptyNet.post(Bob, "Message 1").post(Bob, "Message 2")
+    assert(updatedNet.read(Bob) == List("Message 2", "Message 1"))
   }
 }
