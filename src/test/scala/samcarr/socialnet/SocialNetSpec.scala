6bbba3d42213bc81b0b4f378e2372312a7cb3f44
Sam Carr <samcarr@me.com>
2014-06-27 09:35:58 +0100

    Better comment and use the fake clock.

--- src/test/scala/samcarr/socialnet/SocialNetSpec.scala
+++ src/test/scala/samcarr/socialnet/SocialNetSpec.scala
@@ -7,39 +7,25 @@ import scala.language.implicitConversions
 
 class SocialNetSpec extends UnitSpec {
   
-  class IncrementingTimeSource(var time: Long) extends TimeSource {
-    override def currentTime() = {
-      val current = new Date(time)
-      time += 1
-      current
-    }
-  }
-  
-  def timeZeroedNet() = {
-    implicit val fakeTimeSource = new IncrementingTimeSource(0L)
-    SocialNet()
-  }
+  val emptyNet = SocialNet()
   
   "SocialNet" should "list zero messages for a user that has never posted" in {
-    val emptyNet = timeZeroedNet()
     assert(emptyNet.read(Bob) === List())
   }
   
   it should "allow a user to post a message and read it back" in {
-    val emptyNet = timeZeroedNet()
     val net = emptyNet.post(Bob, "A message.")
     assertMessageListsEqual(net.read(Bob), Bob -> "A message.")
   }
   
   it should "list posted messages for a user, newest first" in {
-    val emptyNet = timeZeroedNet()
     val net = emptyNet.post(Bob, "Message 1")
                       .post(Bob, "Message 2")
     assertMessageListsEqual(net.read(Bob), Bob -> "Message 2", Bob ->"Message 1")
   }
   
   it should "list followed users' messages on a user's wall, interleaved in correct order" in {
-    val emptyNet = timeZeroedNet()
+    val emptyNet = timeIncrementingNet()
     val net = emptyNet.follow(Alice, Bob)
                       .post(Alice, "A1")
                       .post(Bob, "B1")
@@ -52,5 +38,21 @@ class SocialNetSpec extends UnitSpec {
     // Compare lists of pairs so we get helpful failure messages out.
     val actualPairs = actual map { case Message(u, m, _) => (u, m) }
     assert(actualPairs == expected.toList)
+  }
+  
+  // The system clock does not have the granularity to result in predictable
+  // ordering of messages (we end up with same time on all of them) so we
+  // provide a predictable, incrementing clock that can be used instead.
+  class IncrementingTimeSource(var time: Long) extends TimeSource {
+    override def currentTime() = {
+      val current = new Date(time)
+      time += 1
+      current
+    }
+  }
+  
+  def timeIncrementingNet() = {
+    implicit val fakeTimeSource = new IncrementingTimeSource(0L)
+    SocialNet()
   }
 }
